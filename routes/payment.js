c6869a06da49b5cfb50550de5543aea51679babf
soft26wala <soft26wabdev@gmail.com>
2026-01-04 09:27:27 +0530

    app v1.4.0

--- routes/payment.js
+++ routes/payment.js
@@ -2,6 +2,8 @@ import express from "express";
 import Razorpay from "razorpay";
 import crypto from "crypto";
 import { connectDB } from "../db/db.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const router = express.Router();
 
